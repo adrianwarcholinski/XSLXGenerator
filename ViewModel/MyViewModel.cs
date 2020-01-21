efc95f9a4c2bc32a521389cd16f8014d14d094a2
adrianwarcholinski <adrianwarcholinski9@gmail.com>
2020-01-21 20:00:17 +0100

    Disable generate button when file is not selected

--- ViewModel/MyViewModel.cs
+++ ViewModel/MyViewModel.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Windows.Input;
 
 namespace ViewModel
@@ -22,37 +21,65 @@ namespace ViewModel
 
         #endregion Commands
 
+        #region  Properties
         public string SelectedFileName
         {
             get => _selectedFileName;
             set
             {
                 _selectedFileName = value;
+                IsActiveGenerateButton = !string.IsNullOrEmpty(value);
                 RaisePropertyChanged();
             }
         }
+
+        public bool IsActiveGenerateButton
+        {
+            get => _isActiveGenerateButton;
+            set
+            {
+                _isActiveGenerateButton = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion Properties
+
 
         public MyViewModel()
         {
             SelectFileCommand = new Command(SelectFile);
         }
 
+        #region Raise PropertyChanged
         protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        #endregion
+
+        #region Commands methods
         public void SelectFile()
         {
             string fileName = "";
-            _selectedFile = Window.SelectFile(ref fileName);
-            SelectedFileName = fileName.Replace("_", "__");
+
+            Stream file = Window.SelectFile(ref fileName);
+            if (file != null)
+            {
+                _selectedFile = file;
+                SelectedFileName = fileName.Replace("_", "__");
+
+            }
         }
+
+        #endregion Commands methods
 
         #region Private stuff
 
         private string _selectedFileName;
         private Stream _selectedFile;
+        private bool _isActiveGenerateButton;
 
         #endregion Private stuff
     }
